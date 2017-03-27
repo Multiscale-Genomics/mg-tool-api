7e1711e39e79c3d3b314720121f9f6e82bca7728
Mark McDowall <mark.mcdowall@gmail.com>
2017-03-27 11:15:27 +0100

    Added module details

--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='mgToolApi',
+    
+    version='0.3',
+    description='MuG Tool API',
+    
+    url='http://www.multiscalegenomics.eu',
+    download_url='https://github.com/Multiscale-Genomics/mg-tool-api',
+    
+    author='Marco Pasi, Javier Conejero',
+    author_email='',
+
+    license='GPL v2',
+
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
