2dca7dd449740e2f8c2001feff5a6387eea5515a
Mark McDowall <mark.mcdowall@gmail.com>
2017-03-27 09:53:57 +0100

    Modified the name to remove the dashes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mg-tool-api',
+    name='mgToolApi',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
