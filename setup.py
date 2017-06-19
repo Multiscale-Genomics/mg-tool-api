04365953c4fc1b3aab34421efd2852370d3179d5
Mark McDowall <mark.mcdowall@gmail.com>
2017-06-19 09:55:01 +0100

    Setup files for installation with pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mg-tool-api',
+    version='0.3',
+    description='MuG DMP API',
+    
+    url='http://www.multiscalegenomics.eu',
+    download_url='https://github.com/Multiscale-Genomics/mg-tool-api',
+    
+    author='Marco Pasi, Javier Conejero',
+    #author_email='',
+    
+    license='Apache 2.0',
+    
+    packages=find_packages(),
+    
+    install_requires = [
+        'configparser', 'pytest'
+    ],
+
+    tests_require=[
+        'pytest',
+    ],
+)
