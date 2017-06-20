b7df1ae9fec0e81e3a9589158163fd15ca36effb
Mark McDowall <mark.mcdowall@gmail.com>
2017-06-20 09:43:58 +0100

    Modified to include extra extensions

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,13 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    #'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode'
+]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
