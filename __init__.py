60c99e80eb016cabe7a857635e9b9723ef2110f1
Mark McDowall <mark.mcdowall@gmail.com>
2017-06-29 16:29:11 +0100

    Root init

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (C) 2016 Marco Pasi <mf.pasi@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+"""
+
+__author__ = 'Marco Pasi, Javier Conejero'
+__version__ = 'v0.5'
+__license__ = 'GPL v2'
