9ce2a70fb47f24fdc71ad26a2ca2209adfa0b5ab
Mark McDowall <mark.mcdowall@gmail.com>
2017-08-02 10:32:47 +0100

    Minor change to the init to test for RTD

---
+++ basic_modules/__init__.py
@@ -0,0 +1,9 @@
+"""
+__init__ for teh basic modules
+"""
+
+from .app import App
+from .datatypes import Datatypes
+from .metadata import Metadata
+from .tool import Tool
+from .workflow import Workflow
