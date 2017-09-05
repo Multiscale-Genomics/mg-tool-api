db058b60890987de08889886c849e03494b6facb
Marco Pasi <mfpasi@gmail.com>
2017-09-05 14:54:09 +0100

    WIP: Skeleton for App JSON read/write

--- basic_modules/app.py
+++ basic_modules/app.py
@@ -1,6 +1,7 @@
 # from mug import datatypes as mug_datatypes
-from datatypes import Datatypes as mug_datatypes
 from metadata import Metadata
+from basic_modules import input_file_spec, output_file_spec
+import json
 
 
 # -----------------------------------------------------------------------------
@@ -17,9 +18,8 @@ class App(object):
 
     In general, App deals with:
 
-    1) instantiate and configure the Tool,
-    2) call its "run" method and
-    3) deal with errors
+    1) instantiate and configure the Tool, and
+    2) call its "run" method
 
     The App.launch() method is called in order to run a Tool within the App,
     with each call wrapping a single Tool class. The App.launch method calls
@@ -28,13 +28,7 @@ class App(object):
     facilitate the accumulation of features in App subclasses in a way similar
     to the mixin pattern (see for example WorkflowApp).
 
-    The App must check for errors in Tool execution and take the necessary
-    actions to report the errors to the caller. Since Tools may generally be
-    executed on separate machines, the Exceptions are passed by the Tools in
-    the Metadata (see Metadata.set_exception). The App._error_report() method
-    should be called to report errors.
-
-    As Apps need to be compatible with any Tool, it is unpractical to use Apps
+    As Apps need to be compatible with any Tool, it is impractical to use Apps
     to combine Tools. Instead, Workflows can be implemented (see Workflow) in
     order to take advantage of the VRE's capabilities to optimise the data flow
     according to the specific requirements of the workflow, by ensuring that
@@ -45,63 +39,55 @@ class App(object):
     see PyCOMPSsApp).
     """
 
-    def launch(self, tool_class,
-               input_files, input_metadata,
-               output_files, configuration):
+    def launch(self, tool_class, config_json, results_json):
         """
-        Run a Tool with the specified inputs and configuration.
+        Run a Tool with the specified config_json and write
+        result information to results_json.
 
 
         Parameters
         ----------
         tool_class : class
             the subclass of Tool to be run;
-        input_ids : list
-            a list of unique IDs of the input data elements required by the
-            Tool;
-        configuration : dict
-            a dictionary containing information on how the tool should be
-            executed.
-
+        config_json : string
+            path to a valid JSON file containing information on how the tool
+            should be executed. The schema for this JSON string is the
+            "config.json".
+        results_json : string
+            path where to write the JSON file containing information on the
+            results of the tool execution. The schema for this JSON string is
+            the "results.json".
 
         Returns
         -------
-        list
-            A list of unique IDs for the Tool's output data elements.
+        bool
 
 
         Example
         -------
         >>> import App, Tool
         >>> app = App()
-        >>> app.launch(Tool, [<input_id>], {})
+        >>> # expects to find valid config.json
+        >>> app.launch(Tool, "config.json", "results.json")
+        >>> # writes results.json
         """
 
+        print "0) Unpack information from JSON"
+        input_files, output_files, arguments = self._read_json(config_json)
+
         print "1) Instantiate and configure Tool"
-        tool_instance = self._instantiate_tool(tool_class, configuration)
+        tool_instance = self._instantiate_tool(tool_class, arguments)
 
         print "2) Run Tool"
-        input_files, input_metadata = self._pre_run(tool_instance,
-                                                    input_files,
-                                                    input_metadata)
+        input_files, output_files = self._pre_run(tool_instance,
+                                                  input_files, output_files)
 
-        output_files, output_metadata = tool_instance.run(input_files,
-                                                          input_metadata,
-                                                          output_files)
+        output_files = tool_instance.run(input_files, output_files)
 
-        output_files, output_metadata = self._post_run(tool_instance,
-                                                       output_files,
-                                                       output_metadata)
+        output_files = self._post_run(tool_instance, output_files)
 
-        print "3) Check for errors"
-        if any([outmd.error for outmd in output_metadata]):
-            fatal = self._error(
-                [outmd for outmd in output_metadata if outmd.error])
-            if fatal:
-                return None
-
-        print "Output_files: ", output_files
-        return output_files
+        print "3) Pack information to JSON"
+        return self._write_json(results_json)
 
     def _instantiate_tool(self, tool_class, configuration):
         """
@@ -110,37 +96,25 @@ class App(object):
         """
         return tool_class(configuration)
 
-    def _pre_run(self, tool_instance, input_files, input_metadata):
+    def _pre_run(self, tool_instance, input_files, output_files):
         """
         Subclasses can specify here operations to be executed BEFORE running
         Tool.run(); subclasses should also run the superclass _pre_run.
 
         Receives the instance of the Tool that will be run, and its inputs
-        values: input_files and input_metadata (see Tool).
-        Returns input_files and input_metadata.
+        values: input_files and output_files (see Tool).
+        Returns input_files and output_files.
         """
-        return input_files, input_metadata
+        return input_files, output_files
 
-    def _post_run(self, tool_instance, output_files, output_metadata):
+    def _post_run(self, tool_instance, output_files):
         """
         Subclasses can specify here operations to be executed AFTER running
         Tool.run(); subclasses should also run the superclass _post_run.
 
         Receives the instance of the Tool that was run, and its return values:
-        output_files and output_metadata (see Tool).
-        Returns output_files and output_metadata.
+        output_files (see Tool).
+        Returns output_files
         """
-        return output_files, output_metadata
+        return output_files
 
-    def _error(self, error_metadata):
-        """
-        Subclasses can specify here operations to be executed to treat and
-        report errors occurred in Tool.run(). The passed error_metadata should
-        be only instances of Metadata that carry an Exception.
-
-        Returns True if the error is FATAL and entails immediate arrest of App
-        operations (i.e. unstaging will not occur).
-        """
-        for errmd in error_metadata:
-            print errmd.exception
-        return True
